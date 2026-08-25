56b3e2acf6d735dba7abbe9cbef63232c9df1afc
in my mind... <94906343+l0v3m0n3y@users.noreply.github.com>
2026-08-25 18:24:44 +0300

    Add HTTPMethod enum for request methods

--- src/sodeom.swift
+++ src/sodeom.swift
@@ -20,6 +20,13 @@ extension URLSession {
     }
 }
 
+public enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+    case patch = "PATCH"
+}
 
 public class Sodeom {
     private let api = "https://sodeom.com"
